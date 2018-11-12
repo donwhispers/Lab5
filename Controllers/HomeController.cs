dfd650158b1586afaf6dd609a8ad3d936d4af1e5
Aliaksei Hrechushkin <d.cells.aloha@gmail.com>
2018-11-13 00:29:09 +0300

    base functionality

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Lab5.Models;
+using Microsoft.AspNetCore.Authorization;
+
+namespace Lab5.Controllers
+{
+    public class HomeController : Controller
+    {
+        [Authorize]
+        public IActionResult Index()
+        {
+            return Content(User.Identity.Name);
+        }
+    }
+}
